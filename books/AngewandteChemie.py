96eceb1c23bfc08c055a8e970319b0c8c50d8f54
taochem <taoli@sas.upenn.edu>
2017-04-07 17:31:12 -0400

    add anguwaine chemi

---
+++ books/AngewandteChemie.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from base import BaseFeedBook
+
+def getBook():
+    return AngewandteChemie
+
+class AngewandteChemie(BaseFeedBook):
+    title                 = 'AngewandteChemie'
+    description           = 'Angewandte Chemie International Edition'
+    language              = 'en'
+    feed_encoding 		  = "utf-8"
+    page_encoding 		  = "utf-8"
+    mastheadfile 		  = "mh_angewandteChemie.png"
+    coverfile 			  =  'cv_angewandteChemie.png'
+    keep_image			  = True
+    fulltext_by_readability = True
+    oldest_article = 1
+    feeds = [
+            (u'Angewandte Chemie','http://onlinelibrary.wiley.com/rss/journal/10.1002/(ISSN)1521-3773')
+           ]
